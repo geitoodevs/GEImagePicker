fecee5494e8214b791120f42a50feb60d3af7b22
Miguel Developer <miguel@appsonline.es>
2016-02-24 19:59:50 +0100

    Removed pods

--- Example/Pods/Target Support Files/GEImagePicker/GEImagePicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GEImagePicker : NSObject
-@end
-@implementation PodsDummy_GEImagePicker
-@end
--- Example/Pods/Target Support Files/GEImagePicker/GEImagePicker-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double GEImagePickerVersionNumber;
-FOUNDATION_EXPORT const unsigned char GEImagePickerVersionString[];
-
